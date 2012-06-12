be081ef98d4c5b967d203ea0261286d8d2e77449
Tomas Korcak <korczis@gmail.com>
2012-06-13 00:45:51 +0200

    Initial import

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#define UPX_VERSION_HEX         0x030800        /* 03.08.00 */
+#define UPX_VERSION_STRING      "3.08"
+#define UPX_VERSION_STRING4     "3.08"
+#define UPX_VERSION_DATE        "Dec 12th 2011"
+#define UPX_VERSION_DATE_ISO    "2011-12-12"
+#define UPX_VERSION_YEAR        "2011"
